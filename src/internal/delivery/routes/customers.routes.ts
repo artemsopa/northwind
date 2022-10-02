f8ea11fd6f7c1514cd98b29f53ea9f8ff0d5d78e
Artom Sopun <artomsopun@gmail.com>
2022-10-02 14:10:27 +0300

    Add routes

---
+++ src/internal/delivery/routes/customers.routes.ts
@@ -0,0 +1,50 @@
+import {
+  NextFunction, Request, Response, Router,
+} from 'express';
+import { ICustomersService } from '../../services/services';
+import { idReqSchema, customerReqSchema } from './joi-schemas/req.schema';
+import validateSchema from './joi-schemas/schema';
+
+class CustomersRoute {
+  constructor(private customersService: ICustomersService) {
+    this.customersService = customersService;
+  }
+
+  initRoutes() {
+    return Router()
+      .get('/', this.getAll.bind(this))
+      .get('/:id', this.getInfo.bind(this))
+      .get('/search/:company', this.search.bind(this));
+  }
+
+  private async getAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const customers = await this.customersService.getAll();
+      res.status(200).json(customers);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  private async getInfo(req: Request, res: Response, next: NextFunction) {
+    try {
+      const params = validateSchema(idReqSchema, req.params);
+      const customer = await this.customersService.getInfo(params.id);
+      res.status(200).json(customer);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  private async search(req: Request, res: Response, next: NextFunction) {
+    try {
+      const params = validateSchema(customerReqSchema, req.params);
+      const customer = await this.customersService.search(params.company);
+      res.status(200).json(customer);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default CustomersRoute;
