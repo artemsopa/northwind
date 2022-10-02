5324f5f34e7ef3fb6ed88298a345a87448badee5
Artom Sopun <artomsopun@gmail.com>
2022-10-02 14:11:33 +0300

    Add sqs consumer

---
+++ src/internal/delivery/consumer.ts
@@ -0,0 +1,67 @@
+import { SQSEvent } from 'aws-lambda';
+import { IMetricsService } from '../services/services';
+import { ISQSQueue } from '../../pkg/queue/sqs.queue';
+
+class Consumer {
+  constructor(private metricsService: IMetricsService, private queue: ISQSQueue) {
+    this.metricsService = metricsService;
+    this.queue = queue;
+  }
+
+  async handle(sqsEvent: SQSEvent) {
+    const dequeuedMessages = this.mapEventToDequeuedMessages(sqsEvent);
+    console.log(dequeuedMessages);
+    // const messagesToDelete: DequeuedMessage[] = [];
+
+    // const promises = dequeuedMessages.map(async (message) => {
+    //   try {
+    //     await this.processMessage(message);
+    //     messagesToDelete.push(message);
+    //   } catch (error) {
+    //     if (error instanceof NonRetriableError) {
+    //       messagesToDelete.push(message);
+    //       this.logService.error(
+    //         EventService.name,
+    //         'Processing message',
+    //         message,
+    //         'caused a non retriable error. Error:',
+    //         error,
+    //       );
+    //     } else {
+    //       this.logService.error(
+    //         EventService.name,
+    //         'Processing message',
+    //         message,
+    //         'caused a retriable error. Error:',
+    //         error,
+    //       );
+    //     }
+    //   }
+    // });
+    // await until all messages have been processed
+    // await Promise.all(promises);
+
+    // Delete successful messages manually if other processings failed
+    // const numRetriableMessages = dequeuedMessages.length - messagesToDelete.length;
+    // if (numRetriableMessages > 0) {
+    //   await this.queueService.deleteMessages(messagesToDelete);
+
+    //   const errorMessage = `Failing due to ${numRetriableMessages} unsuccessful and retriable errors.`;
+
+    //   throw new PartialFailureError(errorMessage);
+    // }
+  }
+
+  private mapEventToDequeuedMessages(event: SQSEvent) {
+    return event.Records.map((record) => {
+      const message = JSON.parse(record.body);
+      return {
+        id: record.messageId,
+        receiptHandle: record.receiptHandle,
+        ...message,
+      };
+    });
+  }
+}
+
+export default Consumer;
