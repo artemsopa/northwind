0becb141888a394952f1077ce39d2b60a3cc68b7
Artom Sopun <artomsopun@gmail.com>
2022-10-02 14:13:40 +0300

    Add func exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('');
+import { APIGatewayEvent, Context, SQSEvent } from 'aws-lambda';
+import serverless from 'serverless-http';
+import app from './app/app';
+
+export const handler = async (event: APIGatewayEvent, context: Context) => serverless((await app).handler)(event, context);
+
+export const consumer = async (event: SQSEvent) => (await app).consumer.handle(event);
