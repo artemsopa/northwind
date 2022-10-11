3c889dd5a9034f12830b8d6ee653be06fc88cd46
Artom Sopun <artomsopun@gmail.com>
2022-10-11 16:13:02 +0300

    Update to recipinet_id

--- northwind-drizzle-orm/api/src/internal/repositories/entities/employees.ts
+++ northwind-drizzle-orm/api/src/internal/repositories/entities/employees.ts
@@ -10,14 +10,14 @@ export const employees = pgTable('employees', {
   titleOfCourtesy: varchar('title_of_courtesy').notNull(),
   birthDate: date('birth_date', { mode: 'date' }).notNull(),
   hireDate: date('hire_date', { mode: 'date' }).notNull(),
-  address: varchar('adress').notNull(),
+  address: varchar('address').notNull(),
   city: varchar('city').notNull(),
   postalCode: varchar('postal_code').notNull(),
   country: varchar('country').notNull(),
   homePhone: varchar('home_phone').notNull(),
   extension: integer('extension').notNull(),
   notes: text('notes').notNull(),
-  recipientId: varchar('recipientId'),
+  recipientId: varchar('recipient_id'),
 }, (table) => ({
   recipientFk: foreignKey(() => ({
     columns: [table.recipientId],
